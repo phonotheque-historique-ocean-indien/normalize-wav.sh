b8238cb36bb1641654e58ea6e2d6ed32a87b4582
GautierMichelin <gautiermichelin@users.noreply.github.com>
2021-02-19 13:42:52 +0100

    Update normalize-wav.sh

--- normalize-wav.sh
+++ normalize-wav.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [ "$#" -ne 2 ]; then
+    echo "Syntax : normalize-wav.sh original_wave.wav normalized_wave.wav"
+    exit 2
+fi
+
 ffmpeg -i $1 -af volumedetect -f null -y nul &> original.txt
 rm -f $2
 grep "max_volume" original.txt > original1.tmp
@@ -9,3 +14,5 @@ ffmpeg -i $2 -af volumedetect -f null -y nul &> result.txt
 
 #rm original.tmp && rm original1.tmp && rm original2.tmp
 cat result.txt
+
+
