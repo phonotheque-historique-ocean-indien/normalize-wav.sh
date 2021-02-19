9890b1615c8b50f7b9905294e10398227352203a
GautierMichelin <gautiermichelin@users.noreply.github.com>
2021-02-19 13:34:07 +0100

    Create normalize-wav.sh

---
+++ normalize-wav.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ffmpeg -i $1 -af volumedetect -f null -y nul &> original.txt
+rm -f $2
+grep "max_volume" original.txt > original1.tmp
+cat original1.tmp | grep -oP "\d+.\d+ dB" > original2.tmp
+yourscriptvar=$(cat "./original2.tmp")
+ffmpeg -i $1 -af "volume=$yourscriptvar" $2
+ffmpeg -i $2 -af volumedetect -f null -y nul &> result.txt
+
+#rm original.tmp && rm original1.tmp && rm original2.tmp
+cat result.txt
